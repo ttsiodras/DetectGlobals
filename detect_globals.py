8f2756fbc7fcc5f2c0f1b2890a8a18da802931d5
Thanassis Tsiodras <ttsiodras@gmail.com>
2021-04-02 20:02:57 +0200

    This is so hard. Python libclang API apparently doesn't expose all info of C++ API.

--- detect_globals.py
+++ detect_globals.py
@@ -7,6 +7,7 @@ not used anywhere.
 
 import os
 import sys
+from collections import namedtuple
 
 # For mypy static type checks.
 from typing import List, Tuple, Any  # NOQA
@@ -15,6 +16,8 @@ from clang.cindex import (
     CursorKind, TokenKind, Index, TranslationUnit, TranslationUnitLoadError)
 
 from multicore_loop import MultiCoreLoop
+
+LineRange = namedtuple('LineRange', ['a_min', 'b_max'])
 
 
 # For SPARC targets, this is the toolchain prefix
@@ -31,44 +34,62 @@ def process_unit(t_unit: Any, processor):
     f_log = open("log.txt", "w")
     interim: List[str] = []
     last_column = 0
-    function_line_ranges: List[int] = []
+    function_line_ranges: List[LineRange] = []
     is_static = False
 
     def emit_interim(is_static, token):
         if not is_static:
-            check_range_iter = iter(function_line_ranges)
-            inside_function = False
-            while not inside_function:
-                try:
-                    a_min = next(check_range_iter)
-                    b_max = next(check_range_iter)
-                    inside_function = a_min < token.location.line < b_max
-                except StopIteration:
+            inside_function = True
+            for rnge in function_line_ranges:
+                if rnge.a_min <= token.location.line <= rnge.b_max:
                     break
+            else:
+                inside_function = False
         if is_static or not inside_function:
             processor(interim[:])
         while interim:
             interim.pop()
 
+    def add_function_border(lineno, data_pair=[]):  # pylint: disable=W0102
+        if not data_pair:
+            data_pair.append(lineno)
+            return
+        function_line_ranges.append(
+            LineRange(data_pair[0], lineno))
+        data_pair.pop()
+
     for node in t_unit.cursor.walk_preorder():
         if node.kind == CursorKind.TRANSLATION_UNIT or     \
                 node.location.file is None or              \
                 node.location.file.name != t_unit.spelling:
+            # print("Skipping over", node.spelling)
             continue
 
         for token in node.get_tokens():
-            f_log.write('%s %s %s\n' % (
-                node.kind, token.kind, token.spelling))
+            f_log.write('%s (%s:%d:%d) %s (%s:%d:%d)\n' % (
+                node.kind,
+                node.spelling,
+                node.location.line,
+                node.location.column,
+                token.kind,
+                token.spelling,
+                token.location.line,
+                token.location.column))
             if node.kind == CursorKind.FUNCTION_DECL and \
                     token.kind == TokenKind.PUNCTUATION:
+                # To separate VAR_DECLs that happen inside functions
+                # from those happening outside, count the braces;
+                # when we are at the outermost level, we are at
+                # global scope.
                 if token.spelling == "{":
                     brace_level += 1
                     if brace_level == 1:
-                        function_line_ranges.append(token.location.line)
+                        # So basically store
+                        add_function_border(token.location.line)
                 elif token.spelling == "}":
                     brace_level -= 1
                     if brace_level == 0:
-                        function_line_ranges.append(token.location.line)
+                        add_function_border(token.location.line)
             elif node.kind == CursorKind.VAR_DECL:
                 if token.kind in [
                         TokenKind.KEYWORD, TokenKind.IDENTIFIER,
@@ -80,8 +101,6 @@ def process_unit(t_unit: Any, processor):
                     if token.spelling == 'static':
                         is_static = True
                         continue
-                    # if token.spelling == 'in':
-                    #     import pdb ; pdb.set_trace()
                     if token.location.column < last_column and \
                             interim:
                         emit_interim(is_static, token)
@@ -147,6 +166,7 @@ def parse_files(list_of_files: List[str]) -> Tuple[Any, List[Any]]:
             # No, parse it now.
             print("[-] %3d%% Parsing " % (
                 100*(i+1)/len(list_of_files)) + filename)
+            # ...to adapt to: .... filename, args=['-I ...', '-D ...']
             t_units.append(idx.parse(filename))
             t_units[-1].save(cache_filename)
     return idx, t_units
--- tests/example2_simpletypes.c
+++ tests/example2_simpletypes.c
@@ -4,6 +4,15 @@ int i;
 float f;
 double d;
 
+// libclang emits the following (node, token) data:
+//
+// CursorKind.VAR_DECL (i) TokenKind.KEYWORD (int)
+// CursorKind.VAR_DECL (i) TokenKind.IDENTIFIER (i)
+// CursorKind.VAR_DECL (f) TokenKind.KEYWORD (float)
+// CursorKind.VAR_DECL (f) TokenKind.IDENTIFIER (f)
+// CursorKind.VAR_DECL (d) TokenKind.KEYWORD (double)
+// CursorKind.VAR_DECL (d) TokenKind.IDENTIFIER (d)
+
 int main()
 {
     int l_i;
