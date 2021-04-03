8396dacb1733737bc924043d4ab6079d37f45157
Thanassis Tsiodras <ttsiodras@gmail.com>
2021-04-03 11:52:43 +0200

    Check reported source filename; and refactor common code out.

--- test_detector.py
+++ test_detector.py
@@ -1,48 +1,66 @@
 from detect_globals import get_globals_of
 
+def common(source, results):
+    for res1, res2 in zip(results, get_globals_of([source])):
+        # Verify, for each result:
+        #
+        # - whethere it is global/static
+        # - name of variable
+        # - type of variable (global/static)
+        assert res1 == res2[:3]
+        # - ...and the reported filename
+        assert res2[3].endswith(source)
+
 def test1_nothing():
     assert get_globals_of(["tests/example1_empty.c"]) == []
 
 def test2_simpletypes():
-    assert get_globals_of(["tests/example2_simpletypes.c"]) == [
+    source = "tests/example2_simpletypes.c"
+    results = [
         ['global', 'i', 'int'],
         ['global', 'f', 'float'],
         ['global', 'd', 'double']
     ]
+    common(source, results)
 
 def test3_typedef():
-    assert get_globals_of(["tests/example3_typedef.c"]) == [
+    source = "tests/example3_typedef.c"
+    results = [
         ['global', 'g_foo', 'Foo']
     ]
+    common(source, results)
 
 def test4_taste1():
-    res = get_globals_of(["tests/example4_taste1.c"])
-    assert res == [
+    source = "tests/example4_taste1.c"
+    results = [
         ['global', 'g1', 'FILE *'],
         ['global', 'g2', 'FILE *'],
         ['global', 'g3', 'FILE *'],
         ['global', 'g_count', 'int'],
         ['global', 'g_brave_fpga', 'int'],
         ['static', 's_someStatic', 'int']]
+    common(source, results)
 
 def test5_taste2():
-    res = get_globals_of(["tests/example5_taste2.c"])
-    assert res == [
+    source = "tests/example5_taste2.c"
+    results = [
         ['global', 'ads', 'FILE *'],
         ['global', 'adb', 'FILE *'],
         ['global', 'async_ads', 'FILE *'],
         ['global', 'async_adb', 'FILE *'],
         ['global', 'contains_sync_interface', 'int']]
+    common(source, results)
 
 def test6_taste3():
-    res = get_globals_of(["tests/example6_taste3.c"])
-    assert res == [
+    source = "tests/example6_taste3.c"
+    results = [
         ['global', 'adb', 'FILE *'],
         ['global', 'ads', 'FILE *']]
+    common(source, results)
 
 def test7_taste4():
-    res = get_globals_of(["tests/example7_taste4.c"])
-    assert res == [
+    source = "tests/example7_taste4.c"
+    results = [
         ['global', 'thread', 'FILE *'],
         ['global', 'process', 'FILE *'],
         ['global', 'nodes', 'FILE *'],
@@ -53,3 +71,4 @@ def test7_taste4():
         ['global', 'system_root_node', 'char *'],
         ['global', 'features_declared', 'int'],
         ['global', 'system_connections_declared', 'unsigned int']]
+    common(source, results)
